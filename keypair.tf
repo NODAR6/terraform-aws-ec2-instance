fd81953c72f32f326908c4efb51d9c6365015a9e
NODAR6 <nodarogbaidze1911@gmail.com>
2024-05-04 10:42:17 -0500

    varibles complited

--- keypair.tf
+++ keypair.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "terraform6" {
   key_name_prefix = "nodar-"
-  public_key      = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC2z76yMPLdbZjjL4G02bkLrFRcnoqVWwB80KdtG3VbfMj+o1aqy5q1x5/d61KkbB4wAk012L9e8CfAx1jqEZbrvKAXi+GgLAEzrVaViy67Wejqj7rVUFKoG3nk4EHjyhBqwdVscinBF2x7F/nECG00WFnTZ3vKZlCKgCqSPfUd7ZBpyelXycerJ7ryLaFPg7akcRw4rWy1foM+D5pDvFgQTAm8WIHLJqHm0cnpmAUI6DUQLnWWLc2BaGUW0dqjW4xiBj/bu2B0v4pLH3EvLZeCsv+ZKlH2aooap8xd4jxmFc72NS5YhWPdNqM2fM7BWT124EJgXfn7+WVZSM85R9HtqQCvuCPQodb+CMGhb6M9GQjXLh8L3ZQU0rSp97d5/QIG70Gdoc/NozJulG/2xnOUdvNwoQCR9KPlDRJLzOW9xq+j1LK1QFuxRvKjaXkTd7EVlxPXcbE2gEAcQLBkNuuHCWs/ciC7lrBJfDRAdjU9h1EeaiIajX6cVL2gMp82JmM= ogbai@Samsung"
+  public_key      = var.public_key
 }
